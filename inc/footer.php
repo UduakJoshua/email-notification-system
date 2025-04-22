cef4e63c9cfbe047bcac7715b1d44e8a31751767
Uduak Joshua <uduakjoshua@gmail.com>
2025-04-22 17:03:29 +0100

    Added a register form

---
+++ inc/footer.php
@@ -0,0 +1,13 @@
+<footer>
+        <div class="container text-center mt-5 mb-5">
+            <p>&copy; 2023 EMS. All rights reserved.</p>
+            <p>Follow us on:
+            <a href="https://www.blessedchildrenacademy.com" class="text-decoration-none">Facebook</a>,
+            <a href="#" class="text-decoration-none">Twitter</a>,
+            <a href="#" class="text-decoration-none">Instagram</a>
+            </p>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ register.php
@@ -0,0 +1,31 @@
+<?php 
+include_once 'inc/header.php';
+?>
+
+<div class="container">
+<h1>Register</h1>
+<form action="register.php" method="post">
+    <div class="mb-3">
+        <label for="username" class="form-label">Username</label>
+        <input type="text" class="form-control" id="username" name="username" required>
+    </div>
+    <div class="mb-3">
+        <label for="email" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="email" name="email" required>
+    </div>
+    <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" class="form-control" id="password" name="password" required>
+    </div>
+    <button type="submit" class="btn btn-primary">Register</button>
+</div>
+
+
+
+
+
+
+<?php
+include_once 'inc/footer.php';
+?>
+   
