75d3680d046b668aa7a5734967abe43ed8e051d7
Uduak Joshua <uduakjoshua@gmail.com>
2025-04-22 16:47:59 +0100

    modified the index page to include a register button

--- index.php
+++ index.php
@@ -27,10 +27,10 @@
           </ul>
         </div>
       </div>
-      <div>
-        <button>
-            <a href="login.php" class="btn btn-primary">Login</a>
-        </button>
+      <div class="d-flex justify-content-end" style="margin-right: 20px;">
+        
+            <a href="register.php" class="btn btn-primary">Register</a>
+            <a href="login.php" class="btn btn-secondary">Login</a>
       </div>
     </nav>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
